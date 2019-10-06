4ba06f3dfa9ea9462d0301b5ed6832977cc77e3f
srkkhan <srkkhan56@yahoo.com>
2019-10-06 11:28:35 +0530

    remove unnecessary files

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,13 +1,11 @@
 import { SET_CURRENT_USER, USER_LOADING } from "../actions/types";
-import {FETCH_PRODUCTS_PENDING, FETCH_PRODUCTS_SUCCESS, FETCH_PRODUCTS_ERROR} from '../actions/types';
 
 const isEmpty = require("is-empty");
 
 const initialState = {
   isAuthenticated: false,
   user: {},
-  loading: false,
-  products:[]
+  loading: false
 };
 
 export default function(state = initialState, action) {
@@ -23,31 +21,7 @@ export default function(state = initialState, action) {
         ...state,
         loading: true
       };
-      case FETCH_PRODUCTS_PENDING: 
-            return {
-                ...state,
-                pending: true
-            }
-        case FETCH_PRODUCTS_SUCCESS:
-          console.log('in3');
-          console.log(action.products);
-          
-          return {
-            ...state,
-            products: action.products
-        }
-        
-        case FETCH_PRODUCTS_ERROR:
-            return {
-                ...state,
-                pending: false,
-                error: action.error
-            }
     default:
       return state;
   }
 }
-
-export const getProducts = state => state.products;
-export const getProductsPending = state => state.pending;
-export const getProductsError = state => state.error;
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
-import productsReducer from "./productsReducer"; 
+
 
 export default combineReducers({
   auth: authReducer,
